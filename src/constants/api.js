2e522537a812c6f646df1c9c774bdc565fcb8438
Lorety-VL <vladimirking18@gmail.com>
2025-05-31 06:05:40 +0300

    add env

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,6 +1,6 @@
 export const API_URLS = {
-  USER_PROFILE: 'http://localhost:3000/api/v1/users',
-  CURRENT_USER: 'http://localhost:3000/api/v1/users/me',
-  LAWYERS: 'http://localhost:3000/api/v1/lawyers',
-  REFRESH_TOKEN: 'http://localhost:3000/api/v1/auth/refresh',
+  USER_PROFILE: `${import.meta.env.VITE_API_URL}/api/v1/users`,
+  CURRENT_USER: `${import.meta.env.VITE_API_URL}/api/v1/users/me`,
+  LAWYERS: `${import.meta.env.VITE_API_URL}/api/v1/lawyers`,
+  REFRESH_TOKEN: `${import.meta.env.VITE_API_URL}/api/v1/auth/refresh`,
 };
